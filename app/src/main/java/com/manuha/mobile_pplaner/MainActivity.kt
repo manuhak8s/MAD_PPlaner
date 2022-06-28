6994aef4cd5e6a6480d1505c103903222439d8d7
manuhak8s <manuel.haugg.business@gmail.com>
2022-06-28 11:06:48 +0200

    added project

---
+++ app/src/main/java/com/manuha/mobile_pplaner/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.manuha.mobile_pplaner
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.manuha.mobile_pplaner.ui.theme.Mobile_PPlanerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Mobile_PPlanerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("User")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Mobile_PPlanerTheme {
+        Greeting("Android")
+    }
+}
